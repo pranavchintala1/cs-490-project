f0d838fee31ddab6893bd2b41b02ae32acf03757
antwas257 <antwas257@gmail.com>
2025-10-17 19:19:28 -0400

    Adding login and home page files. Added navigation file for navigation. That's probably mostly done

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import Nav from "../frontend/src/tools/Nav";
+
+
+
+const Home = ({user}) => {
+  useEffect(() => {
+    if (user?.isLoggedIn) {
+      // display something if the user is logged in, hide it otherwise.
+    }
+  }, [user, session]);
+
+
+  return (
+
+
+
+
+
+
+  )
+
+
+}
