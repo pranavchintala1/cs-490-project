8320c903daedccbfc6c77f53b86ef2fbe92ab16a
antwas257 <antwas257@gmail.com>
2025-10-18 20:59:14 -0400

    Adding navigation to different pages via routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,28 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Home from "../src/pages/home";
+import Login from "../src/pages/login"
+import Register from "../src/pages/register"
+import Auth from "../tools/auth"; //Does nothing... yet....
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
+            <Routes>
+              <Route path = "/" element = {<Home />} />
+              <Route path = "/register" element = {<Register />} />
+              <Route path = "/login" element = {<Login />} />
+
+            </Routes>
+       
+
       </header>
     </div>
   );
