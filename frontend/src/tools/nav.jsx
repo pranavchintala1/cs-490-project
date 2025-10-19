b3e1509c348c37b0d4144fa1b1c36d7bf8cf6be8
antwas257 <antwas257@gmail.com>
2025-10-19 15:24:50 -0400

    Added navigation bar. Will it work? We shall see.

---
+++ frontend/src/tools/nav.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+export const nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Register</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
