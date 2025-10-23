f307ecea5133901d1e818baaf2741ad9ce46b14f
antwas257 <antwas257@gmail.com>
2025-10-22 23:44:54 -0400

    Removed changes that weren't on my ticket list. Did add password reset form tho.

---
+++ frontend/src/pages/forgotPassword.jsx
@@ -0,0 +1,62 @@
+
+import { useForm } from "react-hook-form";
+import { useFlash } from "../context/flashContext";
+
+
+
+
+const ForgotPassword = () => {
+
+    const {
+       
+        handleSubmit,
+        register,
+        formState: { errors },
+        reset,
+    } = useForm();
+
+    const { flash, showFlash }  = useFlash();
+
+
+    const onSubmit = (data) => {
+
+        //In the future, send data to backend, which will then send it to email via node.js shenanagins (?)
+
+        
+        reset();
+        showFlash('If your email exists, you will receive a password reset link.',"success",5000);
+
+
+
+    };
+
+
+
+
+return (
+    <>
+
+    <form className="Reset" onSubmit={handleSubmit(onSubmit)}>
+
+        <input type="email"
+        {...register("email", { required: true })}
+        placeholder="Email"
+        ></input>
+
+        <input type="submit"></input>
+
+    </form>
+
+
+    </>
+
+
+
+
+);
+
+}
+
+
+
+export default ForgotPassword;
