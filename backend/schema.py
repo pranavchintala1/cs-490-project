77222b93d8bf6d404729361afd7419f1aaff0b3c
Pranav Chintala <pranav.chintala27@gmail.com>
2025-10-28 18:52:45 -0400

    updated schema

--- backend/schema.py
+++ backend/schema.py
@@ -9,9 +9,64 @@ class RegistInfo(BaseModel):
     username: str
     password: str
     email: str
-    name: str | None = None
-    phone_number: str | None = None
-    address: str | None = None
+    name: Optional[str] = None
+    phone_number: Optional[str] = None
+    address: Optional[str] = None
+
+class Skill(BaseModel):
+    uuid: str
+    name: str
+    proficiency: Optional[str] = None
+    category: Optional[str] = None
+
+class Employment(BaseModel):
+    uuid: str 
+    #entry_id: str # uniquely identify entries
+    title: str
+    company: Optional[str] = None
+    location: Optional[str] = None
+    start_date: Optional[str] = None # end?
+    end_date: Optional[str] = None # end?
+    description: Optional[str] = None 
+
+class Education(BaseModel):
+    uuid: str
+    #entry_id: str
+    institution_name: str
+    degree: Optional[str] = None
+    field_of_study: Optional[str] = None
+    graduation_date: Optional[str] = None # date?
+    gpa: Optional[float] = None
+    gpa_private: Optional[bool] = False
+    education_level: Optional[str] = None
+    achievements: Optional[str] = None
+
+class Project(BaseModel):
+    uuid: str
+    # entry_id: str
+    project_name: str
+    description: Optional[str] = None
+    role: Optional[str] = None
+    start_date: Optional[str] = None # date?
+    end_date: Optional[str] = None # date?
+    skills: Optional[list[str]] = None
+    team_size = Optional[int] = None
+    details = Optional[str] = None
+    achievements = Optional[str] = None
+    industry = Optional[str] = None
+    media = Optional[list[str]] = None # url?
+    status = Optional[str] = None
+
+class Certification(BaseModel):
+    uuid: str
+    #entry_id: str
+    name: str
+    issuer: Optional[str] = None
+    date_earned = Optional[str] = None # date?
+    date_expiry = Optional[str] = None # date?
+    cert_number = Optional[str] = None
+    document = Optional[str] = None # url?
+    category = Optional[str] = None
 
 class ProfileSchema(BaseModel):
     username: Optional[str] = None
