2b2929bc2a2b9b40662bd1d2e2bc2449628d5b7d
Pranav Chintala <pranav.chintala27@gmail.com>
2025-10-29 16:54:16 -0400

    finished endpoints

--- backend/mongo/employment_dao.py
+++ backend/mongo/employment_dao.py
@@ -1,1 +1,24 @@
 from mongo.dao_setup import db_client, EMPLOYMENT
+
+class EmploymentDAO:
+    def __init__(self):
+        self.collection = db_client.get_collection(EMPLOYMENT)
+
+    async def add_employment(self, uuid, data: dict):
+        return await self.collection.insert_one(data)
+
+    async def retrieve_all_employment(self, uuid: str):
+        return await self.collection.find({"uuid": uuid})
+
+    async def retrieve_employment(self, entry_id: str):
+        return await self.collection.find({"uuid": entry_id})
+
+    async def update_employment(self, entry_id: str, data: dict):
+        updated = await self.collection.update_one({"_id": entry_id}, {"$set": data})
+        return updated.matched_count
+
+    async def delete_employment(self, entry_id: str):
+        result = await self.collection.delete_one({"_id": entry_id})
+        return result.deleted_count
+
+employment_dao = EmploymentDAO()
