df9f35d4074ac38b456d381a4255da16afb83fd6
Pranav Chintala <pranav.chintala27@gmail.com>
2025-11-05 19:18:18 -0500

    added all routers

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,21 @@ from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 
 from routes.auth import auth_router
+from routes.profiles import profiles_router
+from routes.skills import skills_router
+from routes.projects import projects_router
+from routes.employment import employment_router
+from routes.certifications import certifications_router
 
 app = FastAPI()
 
 api_prefix = "/api"
 
-app.include_router(auth_router, prefix = api_prefix)
-# ... include more routers here
+app.include_router(auth_router, prefix = api_prefix) # FIXME: overhaul auth endpoints
+app.include_router(profiles_router, prefix = api_prefix)
+app.include_router(skills_router, prefix = api_prefix)
+app.include_router(projects_router, prefix = api_prefix)
+app.include_router(employment_router, prefix = api_prefix)
+app.include_router(certifications_router, prefix = api_prefix)
+
+# TODO: jobs, resumes?
