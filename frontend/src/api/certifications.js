d233ce225dd3f1d31b2f6afae3149e09439b9c76
pranavchintala1 <pranav.chintala27@gmail.com>
2025-11-08 03:41:03 -0500

    finished api for now

---
+++ frontend/src/api/certifications.js
@@ -0,0 +1,52 @@
+import api from "./base";
+
+const BASE_URL = "/certifications";
+
+class CertificationsAPI {
+    add(data) {
+        return api.post(BASE_URL, data);
+    }
+
+    get(certificationId) {
+        return api.get(`${BASE_URL}?certification_id=${certificationId}`);
+    }
+
+    getAll() {
+        return api.get(`${BASE_URL}/me`);
+    }
+
+    update(certificationId, data) {
+        return api.put(`${BASE_URL}?certification_id=${certificationId}`, data);
+    }
+
+    delete(certificationId) {
+        return api.delete(`${BASE_URL}?certification_id=${certificationId}`);
+    }
+
+    uploadMedia(certificationId, file) {
+        const formData = new FormData();
+        formData.append("media", file);
+        return api.post(`${BASE_URL}/media?certification_id=${certificationId}`, formData);
+    }
+
+    getMedia(mediaId, download) { 
+        // "download" determines whether to immediately download the file or send it as a blob
+        return api.get(`${BASE_URL}/media`, {params: {download, media_id: mediaId}, responseType: "blob"});
+    }
+
+    getMediaIds(certificationId) {
+        return api.get(`${BASE_URL}/media/ids?certification_id=${certificationId}`);
+    }
+
+    updateMedia(mediaId, file) {
+        const formData = new FormData();
+        formData.append("media", file);
+        return api.put(`${BASE_URL}/media?media_id=${mediaId}`, file);
+    }
+
+    deleteMedia(mediaId) {
+        return api.delete(`${BASE_URL}/media?media_id=${mediaId}`);
+    }
+}
+
+export default new CertificationsAPI();
---
+++ frontend/src/api/projects.js
@@ -0,0 +1,52 @@
+import api from "./base";
+
+const BASE_URL = "/projects";
+
+class ProjectsAPI {
+    add(data) {
+        return api.post(BASE_URL, data);
+    }
+
+    get(projectId) {
+        return api.get(`${BASE_URL}?project_id=${projectId}`);
+    }
+    
+    getAll() {
+        return api.get(`${BASE_URL}/me`);
+    }
+
+    update(projectId, data) {
+        return api.put(`${BASE_URL}?project_id=${projectId}`, data);
+    }
+
+    delete(projectId) {
+        return api.delete(`${BASE_URL}?project_id=${projectId}`);
+    }
+
+    uploadMedia(projectId, file) {
+        const formData = new FormData();
+        formData.append("media", file);
+        return api.post(`${BASE_URL}/media?project_id=${projectId}`, formData);
+    }
+
+    getMedia(mediaId, download) { 
+        // "download" determines whether to immediately download the file or send it as a blob
+        return api.get(`${BASE_URL}/media`, {params: {download, media_id: mediaId}, responseType: "blob"});
+    }
+
+    getMediaIds(mediaId) {
+        return api.get(`${BASE_URL}/media/ids?project_id=${mediaId}`);
+    }
+
+    updateMedia(mediaId, file) {
+        const formData = new FormData();
+        formData.append("media", file);
+        return api.put(`${BASE_URL}/media?media_id=${mediaId}`, file);
+    }
+
+    deleteMedia(mediaId) {
+        return api.delete(`${BASE_URL}/media?media_id=${mediaId}`);
+    }
+}
+
+export default new ProjectsAPI();
