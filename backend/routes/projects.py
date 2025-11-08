d9d0dc9f728d185b9cfa0c938059ae4a926edf0c
pranavchintala1 <pranav.chintala27@gmail.com>
2025-11-08 00:00:21 -0500

    added some name changes for semantic clarity

--- backend/routes/projects.py
+++ backend/routes/projects.py
@@ -79,9 +79,9 @@ async def delete_project(project_id: str, uuid: str = Depends(authorize)):
         return {"detail": "Successfully deleted project"}
     
 @projects_router.post("/media", tags = ["projects"])
-async def upload_media(parent_id: str, media: UploadFile = File, uuid: str = Depends(authorize)):
+async def upload_media(project_id: str, media: UploadFile = File, uuid: str = Depends(authorize)):
     try:
-        media_id = await media_dao.add_media(parent_id, media.filename, await media.read(), media.content_type)
+        media_id = await media_dao.add_media(project_id, media.filename, await media.read(), media.content_type)
     except Exception as e:
         raise HTTPException(500, "Encountered interal service error")
     
@@ -109,18 +109,18 @@ async def download_media(media_id, download: bool = False, uuid: str = Depends(a
     )
 
 @projects_router.get("/media/ids", tags = ["projects"])
-async def get_all_media_ids(parent_id:str, uuid: str = Depends(authorize)):
+async def get_all_media_ids(project_id: str, uuid: str = Depends(authorize)):
     try:
-        media_ids = await media_dao.get_all_associated_media_ids(parent_id)
+        media_ids = await media_dao.get_all_associated_media_ids(project_id)
     except Exception as e:
         raise HTTPException(500, "Encountered internal service error")
     
     return {"detail": "Sucessfully gotten media ids", "media_id_list": media_ids}
 
 @projects_router.put("/media", tags = ["projects"])
-async def update_media(parent_id: str, media_id: str, media: UploadFile = File, uuid: str = Depends(authorize)):
+async def update_media(project_id: str, media_id: str, media: UploadFile = File, uuid: str = Depends(authorize)):
     try:
-        updated = await media_dao.update_media(media_id, media.filename, await media.read(), parent_id, media.content_type)
+        updated = await media_dao.update_media(media_id, media.filename, await media.read(), project_id, media.content_type)
     except Exception as e:
         raise HTTPException(500, "Encountered interal service error")
     
