1b1522de2870f64ebefa45f93af56434b29ab068
Pranav Chintala <pranav.chintala27@gmail.com>
2025-11-08 20:15:15 -0500

    updated for cover letter

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from routes.employment import employment_router
 from routes.certifications import certifications_router
 from routes.education import education_router
 from routes.jobs import jobs_router
-from routes.coverLetter import coverletter_router
+from routes.cover_letters import cover_letters_router
 
 app = FastAPI()
 
@@ -44,7 +44,7 @@ app.include_router(education_router, prefix = api_prefix)
 app.include_router(employment_router, prefix = api_prefix)
 app.include_router(certifications_router, prefix = api_prefix)
 app.include_router(jobs_router, prefix = api_prefix)
-app.include_router(coverletter_router)
+app.include_router(cover_letters_router, prefix = api_prefix)
 
 # TODO: add user deletion services (deletes all data, requires password authentication)
 # Where to put it though?
