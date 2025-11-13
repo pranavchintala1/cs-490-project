bd27f7bd8334f608d8b7883fc05c83fc68a91782
jwalp <jonathanwalpow@gmail.com>
2025-11-13 16:35:36 -0500

    Industry thingy

--- frontend/src/pages/jobs/JobList.jsx
+++ frontend/src/pages/jobs/JobList.jsx
@@ -1054,6 +1054,9 @@ const loadJobs = async () => {
               <option value="Education">Education</option>
               <option value="Marketing">Marketing</option>
               <option value="Design">Design</option>
+              <option value="Consulting">Consulting</option>
+              <option value="Manufacturing">Manufacturing</option>
+              <option value="Retail">Retail</option>
               <option value="Other">Other</option>
             </select>
 
@@ -1118,7 +1121,6 @@ const loadJobs = async () => {
             setView("pipeline");
             setEditingJob(null);
             window.location.reload();
-
           }}
         />
       )}
